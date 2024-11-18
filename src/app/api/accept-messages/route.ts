fcaa7388ec385736d58cf5faae59a1635abd9fae
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-11-18 12:18:41 +0530

    Message Status

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -1,1 +1,82 @@
-import { getServerSession } from "next-auth";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option"; 
+import connectDB from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request:Request){
+    await connectDB();
+
+    const session = await getServerSession(authOptions)
+    const user = session?.user
+
+    if(!session || !user){
+        return Response.json({
+            success:false,
+            message:'Not Authenticated'
+        })
+    }
+
+    const userId = user._id;
+    const {acceptMessages} = await request.json()
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            {isAcceptingMessage:acceptMessages},
+            {new:true}
+        )
+        if(!updatedUser){
+            return Response.json({
+                success:false,
+                message:'user not found'
+            })
+        }
+        return Response.json({
+            success: true,
+            message:'Message acceptance status updated successfully'
+        })
+
+    } catch (error) {
+        return Response.json({
+            success:false,
+            message:'Failed to update user status to accept message'
+        })
+    }
+}
+
+export async function GET(request:Request) {
+    await connectDB();
+
+    const session = await getServerSession(authOptions)
+    const user = session?.user
+
+    if(!session || !user){
+        return Response.json({
+            success:false,
+            message:'Not Authenticated'
+        })
+    }
+
+    const userId = user._id;
+
+    try {
+        const foundUser = await UserModel.findById(userId)
+
+        if(!foundUser){
+            return Response.json({
+                success:false,
+                message:'user not found'
+            })
+        }
+        return Response.json({
+            success:true,
+            isAcceptingMessages: foundUser.isAcceptingMessage
+        })
+    
+    } catch (error) {
+        return Response.json({
+            success:false,
+            message:'user not found'
+        })
+    }
+
+}
