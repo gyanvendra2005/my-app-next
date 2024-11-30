3e1194c78ea72fec75de13c7535a6036871c3bff
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-11-30 13:51:43 +0530

    verification of email done

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -102,7 +102,10 @@ import UserModel from '@/model/User';
 import bcrypt from 'bcryptjs';
 import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
 
+
 export async function POST(request: Request) {
+
+
   await connectDB();
 
   try {
