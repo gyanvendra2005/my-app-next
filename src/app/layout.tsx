ed243e5fcd040e9ae7353a090e813081a299853a
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-11-30 18:01:24 +0530

    Email verification added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4371264308889776"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
       <meta name="google-adsense-account" content="ca-pub-4371264308889776"></meta>
       
       <AuthProvider>
